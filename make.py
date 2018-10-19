b7d4489bdfd105dec13c91a146abc842cab9a0e9
nikitastykov <43541437+nikitastykov@users.noreply.github.com>
2018-10-19 09:07:57 +0300

    Update make.py

--- make.py
+++ make.py
@@ -4,7 +4,7 @@ import os
 
 def com_list():
     commands_dictionary= 'command.data'
-    commands = { '!hello':'Hello {0.author.mention}','!about': 'Бот в процессе разработки. функции будут постепенно добавляться. Сейчас работают следующие функции:!hello, !about, !img ,','!img':'cписок изображений :' + str(os.listdir("C:/pytnon_apps/discord bot/ALL_IMAGES/"))};
+    commands = { '!hello':'Hello {0.author.mention}','!about': 'Бот в процессе разработки. функции будут постепенно добавляться. Сейчас работают следующие функции:!hello, !about, !img ,','!img':'cписок изображений :' + str(os.listdir("IMAGES PATH"))};
     f = open(commands_dictionary, 'wb')
     pickle.dump(commands, f)
     f.close()
