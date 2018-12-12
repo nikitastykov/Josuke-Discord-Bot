3816b4c77b289ff31a817ea2bf146c9efdb76860
Nikita <43541437+nikitastykov@users.noreply.github.com>
2018-12-12 23:24:44 +0300

    Update and rename discordv1_2_7.py to discordv1_2_8.py

--- discordv1_2_8.py
+++ discordv1_2_8.py
@@ -10,6 +10,8 @@ import bs4
 import lxml
 import re
 import time
+import apiai
+import json
 
 def d2rucrawl(url):
     print('работаю')
@@ -33,7 +35,7 @@ def d2rucrawl(url):
     img_b=[i+'/'for i in img_a]
     img_c=[''.join(img_b)]
 
-    while pages <10 and pages !=0: #блок расчета количества постов
+    while pages <11 and pages !=0: #блок расчета колличества постов
         html = urlopen(url+'activity/page-'+str(pages))
         soup = BeautifulSoup(html, 'lxml')
         type(soup)
@@ -60,11 +62,24 @@ def d2rucrawl(url):
     activity_end=', '.join(activity_mess)
     return 'Никнейм: ' + username.replace(' ','')[2:-1] + ', Сообщения: '+ messagesn[1:-1] +', Симпатии: ' + likes[1:-1]+' Часто используемые слова  за последние 100 сообщений - '+ activity_end +' Аватар: '+'https://dota2.ru/'+str(img_c)[2:-4]
 
+def textMessage(update):
+    request = apiai.ApiAI('DIALOGFLOAPI').text_request() # Токен API к Dialogflow
+    request.lang = 'ru' # На каком языке будет послан запрос
+    request.session_id = 'Josuke' # ID Сессии диалога (нужно, чтобы потом учить бота)
+    request.query = update #message.text # Посылаем запрос к ИИ с сообщением от юзера
+    responseJson = json.loads(request.getresponse().read().decode('utf-8'))
+    response = responseJson['result']['fulfillment']['speech'] # Разбираем JSON и вытаскиваем ответ
+    # Если есть ответ от бота - присылаем юзеру, если нет - бот его не понял
+    if response:
+        return response
+    else:
+        return 'Я Вас не совсем понял!'
+
 
 discord.__version__
-imglist = os.listdir("IMG PATH HERE")
+imglist = os.listdir("IMAGES PATH")
 
-TOKEN = 'TOKEN HERE'
+TOKEN = 'DISCORD TOKEN'
 
 client = discord.Client()
 connection = sqlite3.connect("replies.db")
@@ -81,19 +96,23 @@ new=[]
 for i in imglist:
     new.append('!' +i[0:-4])
 
-Answer = ('Отстань от меня!','Я занят, не приставай','Информация обо мне доступена по команде !about','DORARARARARARARA!','Break through and beat you up!','Watch your mouth!')
+
+#Answer = ('Отстань от меня!','Я занят, не приставай','Информация обо мне доступена по команде !about','DORARARARARARARA!','Break through and beat you up!','Watch your mouth!')
 @client.event
 async def on_message(message):
-        # чтобы бот не отвечал себе
+     
         if message.author == client.user:
             return
+        #elif client.user.mentioned_in(message) and message.mention_everyone is False:
+          #msg =random.choice(Answer)
+          #await client.send_message(message.channel, msg)
         elif client.user.mentioned_in(message) and message.mention_everyone is False:
-          msg =random.choice(Answer)
-          await client.send_message(message.channel, msg)
+            msg =textMessage(message.content[23:])
+            await client.send_message(message.channel, msg)
         elif message.content =='!img':
             msg = ('cписок изображений : ' +', '.join(new) ).format(message)
             await client.send_message(message.channel, msg)
-        elif message.content.startswith ('!d2'): #выводит инфо по профилю форума
+        elif message.content.startswith ('!d2'):
             u_input=message.content[3:]
             msg = d2rucrawl(u_input).format(message)
             await client.send_message(message.channel, msg)
@@ -109,7 +128,6 @@ async def on_message(message):
         for item in result:
           if message.content.startswith(item):
             await client.add_reaction(message,item[1])
-
 
 
 
